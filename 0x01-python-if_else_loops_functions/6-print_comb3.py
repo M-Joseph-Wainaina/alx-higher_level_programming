031e36410eed4aae19afa1657d4e9327dfc5d17b
joseph wainaina <josephynaina@gmail.com>
2022-12-11 20:45:04 +0000

    print combo created

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i, 10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(99)
+            print(89)
+            break
         else:
             print("{}{}".format(i, j), end=", ")
