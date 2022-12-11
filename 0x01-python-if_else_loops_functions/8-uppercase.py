6a503ac910c2274d91cfacee920e1113ad3ca015
joseph wainaina <josephynaina@gmail.com>
2022-12-11 21:28:44 +0000

    98 battery street

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{}".format(chr(ord(str[i]) - 32)), end='')
-        else:
-            print("{}".format(str[i]), end='')
+        a = ord(str[i])
+        if a >= 97 and a <= 122:
+            a = a - 32
+        print("{}".format(chr(a)), end='')
     print()
+
         
