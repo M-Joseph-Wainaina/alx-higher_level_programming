4f5a8803162e65a6ae54b57cad78b20f41314980
joseph wainaina <josephynaina@gmail.com>
2022-12-18 14:03:19 +0000

    comments added

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,6 +2,10 @@
 import calculator_1
 
 if __name__ == "__main__":
+    """
+    import function from calculator_1.py and does some maths on it
+
+    """
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
