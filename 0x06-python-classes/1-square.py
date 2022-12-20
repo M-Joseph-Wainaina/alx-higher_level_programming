26a15038ae62c8bac50beade2ba951b9baa14e55
joseph wainaina <josephynaina@gmail.com>
2022-12-20 17:33:16 +0000

    func doc updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to store class square"""
+class Square:
+    """class to store the properties and methods of a square"""
+    def __init__(self, size):
+        """__init
+
+        The init method initialises the vale size
+        """
+        self.__size = size
