6d30f0851f212db0f834e65d7fd481a3a3e63c32
joseph wainaina <josephynaina@gmail.com>
2023-01-10 12:50:25 +0000

    comments heavily added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+module to that contains file manipulations
+"""
 
 def read_file(filename=""):
+   """
+   read_file - read a file and print it to stdout
+
+   args:
+        filename - path to the file to be read
+    """
     with open(filename, 'r', encoding="UTF8") as f:
         read_data = f.read()
         print(read_data, end='')
