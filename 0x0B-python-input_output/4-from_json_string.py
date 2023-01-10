b1b467cf25f3fa477cf209fa7a82ba0540c894e6
joseph wainaina <josephynaina@gmail.com>
2023-01-10 19:48:20 +0000

    json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 from json import loads
-"""
-module to convert to a json string to an object
-"""
+
+
 def from_json_string(my_str):
-    """
-    from_json_string - convert a json string into an object
-    args:
-        my_string - string to be converted into a python object
-    """
     return loads(my_str)
