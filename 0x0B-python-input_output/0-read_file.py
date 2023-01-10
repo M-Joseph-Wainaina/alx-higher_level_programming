e75be77a8c84ef67f95c1bf7433ec355eb862d29
joseph wainaina <josephynaina@gmail.com>
2023-01-10 21:48:29 +0000

    pycode style error removed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-"""
-module to that contains file manipulations
-"""
-
-
 def read_file(filename=""):
-    """
-    read_file - readfile and print it to the stdout
-
-    args:
-        filename - path to the file to be read
-    """
-
-    with open(filename, 'r', encoding="UTF8") as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+a = []
+
+for i in range(1, len(sys.argv)):
+    try:
+        a = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        pass
+    finally:
+        a.append(sys.argv[i])
+        save_to_json_file(a, 'add_item.json')
+    
