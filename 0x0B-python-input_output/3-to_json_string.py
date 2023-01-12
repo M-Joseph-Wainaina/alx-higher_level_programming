14e03ae9d3aa310cf1d1bfb84ea3bc1317572b9f
joseph wainaina <josephynaina@gmail.com>
2023-01-12 12:16:26 +0000

    comment added to module and function

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+module to convert an object into a json string
+"""
 from json import dumps
 
 def to_json_string(my_obj):
-    dumps(my_obj)
+   """
+   to convert a py object into a json string
+   """
+   dumps(my_obj)
 
