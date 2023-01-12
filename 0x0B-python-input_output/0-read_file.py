018501750f5a259f469f56b101792880137bf338
joseph wainaina <josephynaina@gmail.com>
2023-01-12 11:36:44 +0000

    module documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,11 @@ module to read a file and print it to the stdout
 
 
 def read_file(filename=""):
-"""
-read_file 
-args:
-    path to file to be read
-"""
+    """
+    read_file 
+    args
+        path to file to be read
+    """
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
