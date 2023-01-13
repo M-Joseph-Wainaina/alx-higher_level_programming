d00ef7f879e7e252b2b3c3fad249e1d3edd63178
joseph wainaina <josephynaina@gmail.com>
2023-01-13 08:56:53 +0000

    pycode style 2.10

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,9 +4,9 @@ module to convert an object into a json string
 """
 from json import dumps
 
-def to_json_string(my_obj):
-   """
-   to convert a py object into a json string
-   """
-   return dumps(my_obj)
 
+def to_json_string(my_obj):
+    """
+    to convert a py object into a json string
+    """
+    return dumps(my_obj)
