456d6d4b9f46527c91f1b19940c17fe0206beab1
joseph wainaina <josephynaina@gmail.com>
2023-01-13 10:11:23 +0000

    doc added to module and func

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+module to import a pyobject from a json file
+"""
 import json
 
 
 def load_from_json_file(filename):
+    """
+    func to load from a json file to a python object
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         return json.loads(f.read())
