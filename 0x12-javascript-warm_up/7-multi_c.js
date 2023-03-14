cbf65b36505d2ad7b1b7d177199fe96beddb94ef
joseph wainaina <josephynaina@gmail.com>
2023-03-14 21:40:32 +0000

    semistandard err fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,10 @@
 const cmdLineArgs = process.argv.slice(2);
 const occurences = Number(cmdLineArgs[0]);
 
-if (!occurences){
-	console.log('Missing number of occurences');
-}
-else {
-	for (let i = 0; i < occurences; i++)
-	{
-		console.log('C is fun');
-	}
+if (!occurences) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < occurences; i++) {
+    console.log('C is fun');
+  }
 }
